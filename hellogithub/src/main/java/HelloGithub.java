1b3815db1bf4e948b70b6f1773359e8d404a61ab
chenqi14 <chenqi14@meituan.com>
2019-03-14 13:21:26 +0800

    hello github

---
+++ hellogithub/src/main/java/HelloGithub.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author chenqi
+ * @date 2019/3/14 下午1:17
+ * @description 第一个github程序
+ **/
+public class HelloGithub {
+
+    public static void main(String[] args){
+        System.out.println("hello github");
+    }
+
+}
